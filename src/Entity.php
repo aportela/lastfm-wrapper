157fc6af1181b3e64c4e03a60d826a8105f3b70f
alex <766f6964+github@gmail.com>
2023-08-04 23:24:53 +0200

    added typed params

--- src/Entity.php
+++ src/Entity.php
@@ -4,10 +4,9 @@ namespace aportela\LastFMWrapper;
 
 class Entity extends \aportela\LastFMWrapper\LastFM
 {
+    public ?string $raw;
 
-    public $raw;
-
-    public function __construct(\Psr\Log\LoggerInterface $logger, string $apiFormat, string $apiKey)
+    public function __construct(\Psr\Log\LoggerInterface $logger, \aportela\LastFMWrapper\APIFormat $apiFormat, string $apiKey)
     {
         parent::__construct($logger, $apiFormat, $apiKey);
         $this->logger->debug("LastFMWrapper\Entity::__construct");
