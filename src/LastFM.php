e109e271bff354c7d9afc8913b46567e4a5d233b
alex <766f6964+github@gmail.com>
2025-11-04 15:52:18 +0100

    added dom extension

--- src/LastFM.php
+++ src/LastFM.php
@@ -14,28 +14,27 @@ class LastFM
     public function __construct(\Psr\Log\LoggerInterface $logger, \aportela\LastFMWrapper\APIFormat $apiFormat, string $apiKey)
     {
         $this->logger = $logger;
-        $this->logger->debug("LastFMWrapper\LastFM::__construct");
         $this->http = new \aportela\HTTPRequestWrapper\HTTPRequest($this->logger, self::USER_AGENT);
         $supportedApiFormats = [\aportela\LastFMWrapper\APIFormat::XML, \aportela\LastFMWrapper\APIFormat::JSON];
         if (!in_array($apiFormat, $supportedApiFormats)) {
-            $this->logger->critical("LastFMWrapper::__construct ERROR: invalid api format");
+            $this->logger->critical("\aportela\LastFMWrapper\LastFM::__construct - ERROR: invalid api format", [$apiFormat, [\aportela\LastFMWrapper\APIFormat::XML->value, \aportela\LastFMWrapper\APIFormat::JSON->value]]);
             throw new \aportela\LastFMWrapper\Exception\InvalidAPIFormat("supported formats: " . implode(", ", [\aportela\LastFMWrapper\APIFormat::XML->value, \aportela\LastFMWrapper\APIFormat::JSON->value]));
         }
         $this->apiFormat = $apiFormat;
         if ($apiFormat == \aportela\LastFMWrapper\APIFormat::XML) {
             $loadedExtensions = get_loaded_extensions();
-            if (!in_array("libxml", $loadedExtensions)) {
-                $this->logger->critical("LastFMWrapper\LastFM::__construct ERROR: libxml extension not found");
-                throw new \aportela\LastFMWrapper\Exception\LibXMLMissingException("loaded extensions: " . implode(", ", $loadedExtensions));
-            } elseif (!in_array("SimpleXML", $loadedExtensions)) {
-                $this->logger->critical("LastFMWrapper\LastFM::__construct ERROR: SimpleXML extension not found");
-                throw new \aportela\LastFMWrapper\Exception\SimpleXMLMissingException("loaded extensions: " . implode(", ", $loadedExtensions));
+            foreach (["dom", "libxml", "SimpleXML"] as $requiredExtension) {
+                if (!in_array($requiredExtension, $loadedExtensions)) {
+                    $this->logger->critical("\aportela\MusicBrainzWrapper\LastFM::__construct - ERROR: {$requiredExtension} php extension not found", $loadedExtensions);
+                    throw new \aportela\LastFMWrapper\Exception\PHPExtensionMissingException("Missing required php extension: {$requiredExtension}, loaded extensions: " . implode(", ", $loadedExtensions));
+                }
             }
             // avoids simplexml_load_string warnings
             // https://stackoverflow.com/a/40585185
             libxml_use_internal_errors(true);
         }
         if (empty($apiKey)) {
+            $this->logger->critical("\aportela\MusicBrainzWrapper\LastFM::__construct - ERROR: empty API KEY");
             throw new \aportela\LastFMWrapper\Exception\InvalidAPIKeyException("empty key");
         }
         $this->apiKey = $apiKey;
@@ -43,6 +42,5 @@ class LastFM
 
     public function __destruct()
     {
-        $this->logger->debug("LastFMWrapper\LastFM::__destruct");
     }
 }
