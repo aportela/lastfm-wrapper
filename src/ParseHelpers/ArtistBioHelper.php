a449984bc37a5875c7dec1c8445339180cc6c883
alex <766f6964+github@gmail.com>
2025-11-14 10:38:50 +0100

    trim "read more" html link on artist bio summary

--- src/ParseHelpers/ArtistBioHelper.php
+++ src/ParseHelpers/ArtistBioHelper.php
@@ -9,4 +9,20 @@ class ArtistBioHelper
     public ?string $summary = null;
 
     public ?string $content = null;
+
+    public function nl2P(string $text, bool $removeDuplicated = true): string
+    {
+        $paragraphs = [];
+        foreach (explode("\n", $text) as $paragraph) {
+            if ($removeDuplicated) {
+                if ($paragraph !== '' && $paragraph !== '0') {
+                    $paragraphs[] = $paragraph = "<p>" . $paragraph . "</p>";
+                }
+            } else {
+                $paragraphs[] = $paragraph = "<p>" . $paragraph . "</p>";
+            }
+        }
+
+        return (mb_trim(implode(PHP_EOL, $paragraphs)));
+    }
 }
--- src/ParseHelpers/XML/ArtistBioHelper.php
+++ src/ParseHelpers/XML/ArtistBioHelper.php
@@ -11,6 +11,15 @@ class ArtistBioHelper extends \aportela\LastFMWrapper\ParseHelpers\ArtistBioHelp
         $children = $element->children();
         if ($children != null) {
             $this->summary = empty($children->summary) ? null : (string) $children->summary;
+            if (!in_array($this->summary, [null, '', '0'], true)) {
+                // trim "Read more on Last.fm" html link
+                $pattern = '/<a href="https:\/\/www\.last\.fm\/.*">Read more on Last.fm<\/a>$/i';
+                $replaced = preg_replace($pattern, "", mb_trim($this->summary));
+                if (is_string($replaced)) {
+                    $this->summary = $this->nl2P(mb_trim($replaced), true);
+                }
+            }
+
             $this->content = empty($children->content) ? null : (string) $children->content;
         } else {
             throw new \aportela\LastFMWrapper\Exception\InvalidXMLException("artist bio element without children elements");
